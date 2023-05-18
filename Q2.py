5a6ef3c67dd77a71d533b6dab6d5123a66d51a3f
YarinBeni <91824149+YarinBeni@users.noreply.github.com>
2023-05-18 15:41:34 +0300

    Create Q2.py
    example of implementation of q2

---
+++ Q2.py
@@ -0,0 +1,63 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Load the data
+df = pd.read_csv('data.csv')
+X = df['X'].values.reshape(-1,1)
+y = df['y'].values
+# Train-Test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# 1. Linear Regression ~~~~~~~~~~~~~~~~~~~
+from sklearn.linear_model import LinearRegression
+# Initialize and fit the model  and predict
+lin_reg = LinearRegression()
+lin_reg.fit(X_train, y_train)
+y_pred = lin_reg.predict(X_test)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# 2. Polynomial Regression ~~~~~~~~~~~~~~~
+from sklearn.preprocessing import PolynomialFeatures
+# Create Polynomial Features
+poly_reg = PolynomialFeatures(degree=4)
+X_poly = poly_reg.fit_transform(X)
+# Initialize and fit the model and predict
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_train, y_train)
+y_pred = lin_reg_2.predict(X_test)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# 3. Random Forest Regression ~~~~~~~~~~~~
+from sklearn.ensemble import RandomForestRegressor
+import xgboost as xgb
+# Initialize and fit the model and predict
+rf_reg = RandomForestRegressor(n_estimators = 1000, random_state = 42)
+rf_reg.fit(X_train, y_train)
+y_pred_rf = rf_reg.predict(X_test)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# 4. XGBoost ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+xg_reg = xgb.XGBRegressor(objective ='reg:squarederror', colsample_bytree = 0.3, learning_rate = 0.1,
+                          max_depth = 5, alpha = 10, n_estimators = 1000)
+xg_reg.fit(X_train, y_train)
+# Predict
+y_pred_xg = xg_reg.predict(X_test)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# 5. Neural Network Methods ~~~~~~~~~~~~~~
+from sklearn.neural_network import MLPRegressor
+# Initialize and fit the model and predict
+nn_reg = MLPRegressor(hidden_layer_sizes=(10, ), activation='relu', solver='adam', learning_rate='adaptive',
+                      max_iter=1000, learning_rate_init=0.01, alpha=0.01)
+nn_reg.fit(X_train, y_train)
+y_pred = nn_reg.predict(X_test)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# 6. Support Vector Machine (SVM) ~~~~~~~
+from sklearn.svm import SVR
+# Initialize and fit the model and predict
+svm_reg = SVR(kernel='rbf', C=100, gamma=0.1, epsilon=.1)
+svm_reg.fit(X_train, y_train)
+y_pred = svm_reg.predict(X_test)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
